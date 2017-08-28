3864e2e780606f509b15a4155803cb8f2fc770ef
Ethcad <brendon-m@outlook.com>
2017-08-28 10:09:00 -0400

    Full relay support, fixed up string parser

--- Autonomous Vehicle Steering/src/Robot.cpp
+++ Autonomous Vehicle Steering/src/Robot.cpp
@@ -11,12 +11,18 @@
 using namespace std;
 
 const bool TRAINING_MODE = true;
-const double ROTATIONS_FROM_MIN_ANGLE_TO_CENTER = 0.3; // How far the motor rotates to turn the wheel to center
+const double ROTATIONS_FROM_MIN_ANGLE_TO_CENTER = 0.37; // How far the motor rotates to turn the wheel to center
 const unsigned int MICROSECONDS_FOR_CALIBRATION_STEP = 3000000;
 
 class Robot: public frc::SampleRobot {
 public:
 	void OperatorControl() {
+		// Turn off the relays
+		SetRelay(communicationRelay, false);
+		SetRelay(recordingRelay, false);
+		SetRelay(autoDriveRelay, false);
+		SetRelay(calibrationRelay, false);
+
 		// Initialize the Talon SRX
 		CANTalon *talon = new CANTalon(0);
 		talon->SetFeedbackDevice(CANTalon::QuadEncoder); // Relative encoder input
@@ -31,47 +37,45 @@ public:
 		talon->SetI(0);
 		talon->SetD(0);
 
-		if (TRAINING_MODE) {
+		if (!calibrated) {
 			// Center the steering wheel, then disable the motor (but not the encoder)
 			talon->ConfigPeakOutputVoltage(4.5, -4.5);
-			bool calibrationSucceeded = CalibrateSteeringWheel(talon);
-			cout << calibrationSucceeded << endl;
+			calibrated = CalibrateSteeringWheel(talon);
 			talon->ConfigPeakOutputVoltage(0, 0);
 		}
+
+		// Enable an indicator relay if calibration succeeded
+		SetRelay(calibrationRelay, calibrated);
 
 		while (IsEnabled()) {
 			cout << "Position: " << talon->GetPosition() << endl;
 			cout << "EncPosition: " << talon->GetEncPosition() << endl;
 			UpdatePositionFile(talon->GetPosition());
-			ReadValuesFromJetsonAndSetRelays();
-//			auto values = ReadValuesFromJetsonAndSetRelays();
-//			bool jetsonEnabled = get<0>(values);
-//			double steeringAngle = get<3>(values);
-//			if (jetsonEnabled)
-//				talon->Set(steeringAngle / 100);
+			ReadSteeringAngleFromJetsonAndSetRelays();
 		}
 	}
 
 private:
-	// Initialize the relays connected to the LEDs
-	Relay *jetsonActiveRelay = new Relay(1);
-	Relay *recordingEnabledRelay = new Relay(2);
+// Initialize the relays connected to the LEDs
+	Relay *recordingRelay = new Relay(0);
+	Relay *communicationRelay = new Relay(1);
+	Relay *autoDriveRelay = new Relay(2);
+	Relay *calibrationRelay = new Relay(3);
 
-	// Counter that ensures the relay doesn't flip on and off with every frame
+// Counter that ensures the relay doesn't flip on and off with every frame
 	const unsigned int jetsonInactiveTimeOut = 20;
 	unsigned int jetsonInactiveTimer = 0;
+
+	bool calibrated = false; // Makes sure we only calibrate once
 
 	bool CalibrateSteeringWheel(CANTalon *talon) {
 		// It is assumed that the wheel starts turned to the maximum in the negative direction
 		talon->SetPosition(ROTATIONS_FROM_MIN_ANGLE_TO_CENTER); // Orient the steering system so that zero lies in the center
 		double almostMaxAngle = 0.9 * ROTATIONS_FROM_MIN_ANGLE_TO_CENTER;
-		usleep(MICROSECONDS_FOR_CALIBRATION_STEP);
 		talon->Set(almostMaxAngle); // Turn almost to the maximum in the positive direction
-
+		usleep(MICROSECONDS_FOR_CALIBRATION_STEP); // Wait for the movement to complete
 		if (abs(talon->Get() - almostMaxAngle) > 0.03) // If we are too far off the desired limit angle
 			return false; // Return failure
-
-		usleep(MICROSECONDS_FOR_CALIBRATION_STEP); // Wait for the movement to complete
 		talon->Set(0); // Turn back to the center
 		usleep(MICROSECONDS_FOR_CALIBRATION_STEP); // Wait for the wheel to center before continuing
 		return true;
@@ -89,13 +93,17 @@ private:
 		system("mv /home/lvuser/temp.encval /home/lvuser/latest.encval");
 	}
 
-	tuple<bool, bool, bool, double> ReadValuesFromJetsonAndSetRelays() {
+	float ReadSteeringAngleFromJetsonAndSetRelays() {
 		// Values for return
-		bool jetsonActive; // Is the Jetson alive and working?
-		bool recordingEnabled; // Is the Jetson recording encoder values?
-		bool autoDrive; // Is the Jetson in autonomous driving mode?
+		bool jetsonActive = false; // Is the Jetson alive and working?
+		bool recordingEnabled = false; // Is the Jetson recording encoder values?
+		bool autoDrive = false; // Is the Jetson in autonomous driving mode?
 		double steeringAngle; // What is the desired steering angle (for auto drive)?
 
+		// Static values that keep track of the values returned from the Jetson the last time communication succeeded.
+		static bool pastRecordingEnabled = false;
+		static bool pastAutoDrive = false;
+		static double pastSteeringAngle = false;
 
 		try {
 			// Attempt to open the file
@@ -104,45 +112,56 @@ private:
 			if (!inFile.good()) {
 				throw runtime_error("Data file does not exist"); // File has not been replaced, fail into the catch block
 			}
-			const char *values[3];
+			string values[3];
 
 			// Place lines of file into array
 			string line;
 			for (int i = 0; getline(inFile, line); i++) {
-				values[i] = line.c_str();
+				cout << line << endl;
+				values[i] = line;
 			}
 
 			system("rm /home/lvuser/values.txt"); // Delete the file, the Jetson should soon replace it with new data
 
 			// Parse proper values from strings
-			recordingEnabled = bool(atoi(values[0]));
-			autoDrive = bool(atoi(values[1]));
-			steeringAngle = atof(values[2]);
+			recordingEnabled = bool(stoi(values[0]));
+			cout << "recording enabled: " << recordingEnabled << endl;
+			autoDrive = bool(stoi(values[1]));
+			steeringAngle = stof(values[2]);
 
 			jetsonActive = true; // If the file can be read and parsed, the Jetson is active
 
 			cout << "File successfully parsed" << endl;
-		}
-		catch (...) {
-			// If there are errors, we say the Jetson is inactive and leave garbage values for everything else
+		} catch (...) {
+			// If there are errors, we print an error message
 			// This can happen when we have deleted the file and it has not yet been replaced
-			jetsonActive = false;
 			cout << "File error caught" << endl;
 		}
 
 		// Set the relays according to the values for return
-		SetRelay(recordingEnabledRelay, recordingEnabled);
+		SetRelay(recordingRelay, recordingEnabled);
+		SetRelay(autoDriveRelay, autoDrive);
 
 		if (jetsonActive) {
-			SetRelay(jetsonActiveRelay, true);
+			pastRecordingEnabled = recordingEnabled;
+			pastAutoDrive = autoDrive;
+			pastSteeringAngle = steeringAngle;
+			SetRelay(communicationRelay, true);
 			jetsonInactiveTimer = 0; // Reset the counter
 		} else {
 			jetsonInactiveTimer++; // Increment the timer
-			if (jetsonInactiveTimer > jetsonInactiveTimeOut) // If we have tried to read the file jetsonInactiveTimeOut times without success
-				SetRelay(jetsonActiveRelay, false); // Turn off the jetsonActiveRelay LED
+			if (jetsonInactiveTimer > jetsonInactiveTimeOut) { // If we have tried to read the file jetsonInactiveTimeOut times without success
+				SetRelay(communicationRelay, false); // Turn off the jetsonActiveRelay LED
+				pastRecordingEnabled = false;
+				pastAutoDrive = false;
+				pastSteeringAngle = false;
+			}
 		}
 
-		return make_tuple(jetsonActive, recordingEnabled, autoDrive, steeringAngle);
+		SetRelay(recordingRelay, pastRecordingEnabled);
+		SetRelay(autoDriveRelay, pastAutoDrive);
+
+		return pastSteeringAngle;
 	}
 
 	void SetRelay(Relay *relay, bool on) {
